c6aa321ac14b45dc193e8c4c408220a50dfb7309
Chad Chaney <cchane4@gmail.com>
2018-07-14 20:06:12 -0500

    changing the math formula/adding special number

--- javascript/lottery.js
+++ javascript/lottery.js
@@ -1,10 +1,17 @@
+var get_one_number = function(){
+  random_one = Math.floor(Math.random() * 25) + 1;
+        lotto_one.push(" "+random_one);
+       document.getElementById("special_number").innerText = lotto_one;
+}
 
 $(document).on('click', function() {
-  lottonumber = [];
+  lotto_four = [];
+  lotto_one = [];
     for (var i = 0; i < 5; i++) {
-        var random = Math.floor(Math.random() * 44) + 1;
-        lottonumber.push(" "+random);
-       document.getElementById("lottery-numbers").innerText = lottonumber;
+        var random_four = Math.floor(Math.random() * 68) + 1;
+        lotto_four.push(" "+random_four);
+       document.getElementById("four_numbers").innerText = lotto_four;
     }
 
+  get_one_number();
 })
