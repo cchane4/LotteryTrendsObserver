f06031f56742a5df8c8797b9a0c98423236b360a
Chad Chaney <cchane4@gmail.com>
2018-07-18 12:23:18 -0500

    changing var to let and const

--- javascript/lottery.js
+++ javascript/lottery.js
@@ -2,22 +2,22 @@ function sortNumber(a,b) {
     return a - b;
 }
 
-var get_one_number = function(){
-  random_one = Math.floor(Math.random() * 25) + 1;
-        lotto_one.push(" "+random_one);
-       document.getElementById("special_number").innerText = lotto_one;
+let get_one_number = function(){
+  let lotto_one = [];
+  const random_one = Math.floor(Math.random() * 25) + 1;
+  lotto_one.push(" "+random_one);
+  document.getElementById("special_number").innerText = lotto_one;
 }
 
 $(document).on('click', function() {
-  lotto_four = [];
-  lotto_one = [];
-    for (var i = 0; i < 5; i++) {
-        var random_four = Math.floor(Math.random() * 68) + 1;
-        lotto_four.push(" "+random_four);
-      lotto_four.sort(sortNumber);
-       document.getElementById("four_numbers").innerText = lotto_four;
-
+  let lotto_four = [];
+  //let lotto_one = [];
+  for (let i = 0; i < 5; i++) {
+    const random_four = Math.floor(Math.random() * 68) + 1;
+    lotto_four.push(" "+random_four);
+    lotto_four.sort(sortNumber);
+    document.getElementById("four_numbers").innerText = lotto_four;
     }
 
   get_one_number();
-})
+});
