36f7825dbff94354e65e725f58eb183cb5e0a7f6
Chad Chaney <cchane4@gmail.com>
2018-07-20 21:34:33 -0500

    refactoring into ES6 code

--- javascript/lottery.js
+++ javascript/lottery.js
@@ -1,23 +1,22 @@
-function sortNumber(a,b) {
-    return a - b;
-}
+const sortNumber = (a,b) => {
+  return a - b;
+};
 
-let get_one_number = function(){
+let get_one_number = () => {
   let lotto_one = [];
   const random_one = Math.floor(Math.random() * 25) + 1;
   lotto_one.push(" "+random_one);
   document.getElementById("special_number").innerText = lotto_one;
 }
 
-$(document).on('click', function() {
+$(document).on('click',() => {
   let lotto_four = [];
-  //let lotto_one = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++){
     const random_four = Math.floor(Math.random() * 68) + 1;
     lotto_four.push(" "+random_four);
     lotto_four.sort(sortNumber);
     document.getElementById("four_numbers").innerText = lotto_four;
     }
-
   get_one_number();
 });
+
