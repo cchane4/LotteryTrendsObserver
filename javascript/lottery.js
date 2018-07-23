ed121fbc9b216a42730153613930e8ea751f1a90
Chad Chaney <cchane4@gmail.com>
2018-07-22 20:44:25 -0500

    added new solution that accomplishes the task

--- javascript/lottery.js
+++ javascript/lottery.js
@@ -1,34 +1,19 @@
-const sortNumber = (a,b) => {
-  return a - b;
-};
+// to be put into sort function
+// const sortNumber = (a,b) => {
+//   return a - b;
+// };
 
-
-let get_one_number = () => {
-  let lotto_one = [];
+// to get the special number 
+ const get_one_number = () => {
+ const lotto_one = [];
   const random_one = Math.floor(Math.random() * 25) + 1;
   lotto_one.push(" "+random_one);
   document.getElementById("special_number").innerText = lotto_one;
 }
 
-$(document).on('click',() => {
-  let lotto_four = [];
-  for (let i = 0; i < 5; i++){
-    const random_four = Math.floor(Math.random() * 68) + 1;
-  lotto_four = [new Set(lotto_four)]; 
-    //lotto_four.push(""+random_four);
-    
+//start of the overall function 
 
-    lotto_four.sort(sortNumber);
-    console.log(lotto_four); 
-}
-  });  
- 
-    /*document.getElementById("four_numbers").innerText = lotto_four;
-    }
-  get_one_number();
-}); */
-
-function getNumbers(amount, minNumber, maxNumber, sorted = true) {
+let getNumbers = (amount, minNumber, maxNumber, sorted = true) => {
   const numbers = [];
   for (let i = 0, l = maxNumber - minNumber + 1; i < l; i += 1) {
   numbers.push(minNumber + i);
@@ -38,5 +23,47 @@ function getNumbers(amount, minNumber, maxNumber, sorted = true) {
   result.push(numbers.splice(Math.floor(Math.random() * numbers.length), 1)[0]);
   }
   return sorted ? result.sort((a, b) => a - b) : result;
-  }
-  getNumbers(5, 1, 68); 
+} 
+
+$(document).on('click',() => {
+  document.getElementById("five_numbers").innerText = getNumbers(5, 1, 68);
+  
+  get_one_number();  
+})
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //lotto_four.sort(sortNumber);
+  // console.log(lotto_four); 
+
+  
+    /*document.getElementById("four_numbers").innerText = lotto_four;
+    }
+  get_one_number();
+}); */
+
+// function getNumbers(amount, minNumber, maxNumber, sorted = true) {
+//   const numbers = [];
+//   for (let i = 0, l = maxNumber - minNumber + 1; i < l; i += 1) {
+//   numbers.push(minNumber + i);
+//   }
+//   const result = [];
+//   for (let i = 0; i < amount; i += 1) {
+//   result.push(numbers.splice(Math.floor(Math.random() * numbers.length), 1)[0]);
+//   }
+//   return sorted ? result.sort((a, b) => a - b) : result;
+//   }
+//   getNumbers(5, 1, 68); 
